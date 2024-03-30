74285a096cb29e1dd48b98dd72237cd3d34512e5
PimKalasinmongkol <pimty.pim@gmail.com>
2024-03-30 19:21:30 +0700

    ver 3

--- src/Page_admin/FirstPage/FirstPage.js
+++ src/Page_admin/FirstPage/FirstPage.js
@@ -1,34 +1,60 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,useParams } from "react-router-dom";
 import { FaCircleUser } from "react-icons/fa6";
-import image from "../../pic/eng.jpg"
-function FirstPage() {
-  const navigate = useNavigate();
+import image from '../../pic/eng.jpg'
+import { useState ,useEffect } from "react";
 
-  const Go_insite = () => {
+function FirstPage({email}) {
+  const navigate = useNavigate();
+  const [data ,setData] = useState([])
+
+  const Go_tosite = () => {
     navigate("/MainPage");
   }
-    return (
-      <div
-      style={{
-        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${image})`, // ใส่ linear gradient เพื่อทำให้รูปพื้นหลังสีจางลง
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center', 
-        height: '100vh',
-      }}
-    >
-      <div className='flex flex-row justify-center h-screen items-center'>
-        <div className='flex flex-col bg-white bg-opacity-70 h-2/4 rounded-lg p-2 mx-12 w-6/12 justify-center relative shadow-md'>
 
-          <div className='flex flex-col items-center py-2 pt-5'>
-            <FaCircleUser size={120} />
-            <div className='flex flex-col p-6 justify-start font-semibold'>
-              <p className='flex pl-3 pb-2 text-rose-color text-2xl'><span>ชื่อ : คุณสมศรี สนุกซุกซน</span></p>
-              <p className='flex pl-3 pt-2 text-rose-color text-2xl'>สถานะ : <p>แอดมิน</p></p>
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`http://localhost:4000/user/getUser`,{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email
+        })
+      })
+      const result = await res.json()
+      setData(result)
+    })()
+  }, [])
+
+  return (
+    <div
+    style={{
+      backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${image})`, // ใส่ linear gradient เพื่อทำให้รูปพื้นหลังสีจางลง
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center', 
+      height: '100vh',
+    }}
+    >
+      <div className='flex flex-row p-1 text-lg justify-center'>
+        <p className=''></p>
+      </div>
+
+      <div className='flex flex-row justify-center my-40'>
+        <div className='flex flex-col bg-white bg-opacity-70 rounded-lg p-2 mx-12 w-6/12 justify-center relative shadow-md'>
+
+        {
+          data.map(item => (
+            <div className='flex flex-col items-center py-2 pt-5'>
+              <FaCircleUser size={120} />
+              <div className='flex flex-col p-6 justify-start'>
+                <p className='flex pl-3 pb-2 text-rose-color text-2xl'>ชื่อ-นามสกุล : <p>{item.name} {item.lastname}</p></p>
+                <p className='flex pl-3 pt-2 text-rose-color text-2xl'>สถานะ : <p>{item.isadmin ? "แอดมิน" : "อาจารย์"}</p></p>
+              </div>
             </div>
-          </div>
+          ))
+          }
           
           <div className="flex justify-end mt-4 mr-4">
-            <button className="bg-white border  border-gray-400 hover:bg-gray-300 text-black font-bold py-1 px-2 rounded" type="button" onClick={Go_insite}>ยินดีต้อนรับเข้าสู่ระบบ</button>
+            <button className="bg-white border border-gray-400 hover:bg-gray-300 text-black font-bold py-1 px-2 rounded" type="button" onClick={Go_tosite}>ยินดีต้อนรับเข้าสู่ระบบ</button>
           </div>
         </div>
       </div>
