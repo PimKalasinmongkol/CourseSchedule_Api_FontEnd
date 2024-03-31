18765607539f47c72174ce1150998a6efd19c781
PimKalasinmongkol <pimty.pim@gmail.com>
2024-03-31 20:04:26 +0700

    ver30

--- src/Page_teacher/FirstTeacher.js
+++ src/Page_teacher/FirstTeacher.js
@@ -8,31 +8,25 @@ function FirstTeacher({email}) {
   const [data ,setData] = useState([])
 
   const Go_tosite = () => {
-    navigate("/MainTeacher");
+    navigate("/MainPage");
   }
 
   useEffect(() => {
     (async () => {
-      const res = await fetch(`http://localhost:4000/user/getUser`,{
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: email
-        })
-      })
+      const res = await fetch(`http://localhost:4000/user/getUser`)
       const result = await res.json()
       setData(result)
     })()
   }, [])
 
-    return (
-      <div
-      style={{
-        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${image})`, // ใส่ linear gradient เพื่อทำให้รูปพื้นหลังสีจางลง
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center', 
-        height: '100vh',
-      }}
+  return (
+    <div
+    style={{
+      backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${image})`, // ใส่ linear gradient เพื่อทำให้รูปพื้นหลังสีจางลง
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center', 
+      height: '100vh',
+    }}
     >
       <div className='flex flex-row p-1 text-lg justify-center'>
         <p className=''></p>
@@ -40,6 +34,7 @@ function FirstTeacher({email}) {
 
       <div className='flex flex-row justify-center my-40'>
         <div className='flex flex-col bg-white bg-opacity-70 rounded-lg p-2 mx-12 w-6/12 justify-center relative shadow-md'>
+
         {
           data.map(item => (
             <div className='flex flex-col items-center py-2 pt-5'>
@@ -51,6 +46,7 @@ function FirstTeacher({email}) {
             </div>
           ))
           }
+          
           <div className="flex justify-end mt-4 mr-4">
             <button className="bg-white border border-gray-400 hover:bg-gray-300 text-black font-bold py-1 px-2 rounded" type="button" onClick={Go_tosite}>ยินดีต้อนรับเข้าสู่ระบบ</button>
           </div>
